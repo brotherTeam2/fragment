75cd66efb025334fbd10617e4cedbb7ceb6b03b8
Vadim <vadimstrakovich@gmail.com>
2025-11-25 15:05:39 +0300

    change file

--- function.js
+++ function.js
@@ -5,10 +5,9 @@ AdaptavistBridge.request({
   url: `/rest/api/2/issue/${AdaptavistBridgeContext.context.issueKey}`,
   type: 'GET'
 })
-  .then(function (issue) {
-    console.log('issue', issue);
-    document.getElementById("col1-value").textContent =
-      `${issue.key} is a ${issue.fields.issuetype.name} in ${issue.fields.status.name}`;
+  .then(issue => {
+        console.log('issue', issue)
+        document.getElementById("col1-value").value = `${issue.key} is a ${issue.fields.issuetype.name} in ${issue.fields.status.name}`;
   })
   .catch(function (e) {
     console.error('Error loading issue', e);
